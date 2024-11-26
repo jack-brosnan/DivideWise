fe02b13aa2dcdeb1ef2eee5372b9f349f12cf57d
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-11-26 11:23:58 +0000

    Added Contributor and Contribution model classes

--- expense_app/models.py
+++ expense_app/models.py
@@ -31,7 +31,7 @@ class ExpenseLine(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    paid_status = models.IntegerField(choices=CURRENCY, default=0)
+    paid_status = models.IntegerField(choices=PAID_STATUS, default=0)
     due_date = models.DateTimeField
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -39,3 +39,21 @@ class ExpenseLine(models.Model):
     class Meta:
         ordering = ["created_on"]
 
+class Contributor(models.Model):
+    """
+    Stores a single contributor line entry related to :model:`expense_app.ExpenseSpace` and :model:`expense_app.Contribution`.
+    """
+    expense_space = models.ForeignKey(ExpenseSpace, on_delete=models.CASCADE, related_name="contributors")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+class Contribution(models.Model):
+    """
+    Stores a single contributor expense line entry related to :model:`expense_app.ExpenseLine`.
+    """
+    expense_line = models.ForeignKey(ExpenseLine, on_delete=models.CASCADE, related_name="expense_contributions")
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE, related_name="expense_contributors")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    custom_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    remaining_share = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    paid_status = models.IntegerField(choices=PAID_STATUS, default=0)
