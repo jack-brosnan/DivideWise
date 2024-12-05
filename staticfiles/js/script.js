86b26a09a926f8aeb1022cf09c3b016d534365de
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-12-05 18:14:18 +0000

    Deployment test with staticfiles loaded

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
     const deleteButtons = document.getElementsByClassName("delete-space");
     const deleteForm = document.getElementById("deleteForm");
-    
+
     for (let button of deleteButtons) {
         button.addEventListener("click", (e) => {
             let spaceId = button.getAttribute("data-space-id");
-            console.log("Space ID:", spaceId); 
-            deleteForm.action = `/delete_space/${spaceId}/`; 
-            console.log("Form Action:", deleteForm.action); 
+            console.log("Space ID:", spaceId);
+            deleteForm.action = `/delete_space/${spaceId}/`;
+            console.log("Form Action:", deleteForm.action);
             deleteModal.show();
         });
     }
@@ -25,4 +25,25 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 1000);
         }
     }, 3000);
+
+    // Toggle expense line accordion dropdown
+
+    const toggleButton = document.getElementById('toggleButton');
+    const panels = document.querySelectorAll('.accordion-panel');
+
+    let status = false;
+
+    toggleButton.addEventListener('click', function () {
+        panels.forEach(panel => {
+            if (status) {
+                panel.classList.remove('show');
+            } else {
+                panel.classList.add('show');
+            }
+        });
+
+        status = !status;
+        toggleButton.textContent = status ? 'Collapse All' : 'Show All';
+    });
+
 });
