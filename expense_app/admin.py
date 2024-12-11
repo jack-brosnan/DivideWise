87ab5ef9c03961b815ee47bcfb8b5e9bb35a7a22
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-12-11 21:09:21 +0000

    Updated admin.py to comply with PEP8

--- expense_app/admin.py
+++ expense_app/admin.py
@@ -3,24 +3,49 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import ExpenseSpace, ExpenseLine, Contributor, Contribution
 
 # Register your models here.
+
+
 @admin.register(ExpenseSpace)
 class ExpenseSpaceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'name', 'currency', 'created_on', 'updated_on')
+    list_display = (
+        'id',
+        'user',
+        'name',
+        'currency',
+        'created_on',
+        'updated_on'
+        )
 
-    
+
 @admin.register(ExpenseLine)
 class ExpenseLineAdmin(admin.ModelAdmin):
-    list_display = ('id', 'expense_space', 'title', 'amount', 'paid_status', 'created_on')
+    list_display = ('id',
+                    'expense_space',
+                    'title',
+                    'amount',
+                    'paid_status',
+                    'created_on')
 
-    
+
 @admin.register(Contributor)
 class ContributorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'expense_space', 'name', 'email')
+    list_display = (
+        'id',
+        'expense_space',
+        'name',
+        'email')
 
-    
+
 @admin.register(Contribution)
 class ContributionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'expense_line', 'contributor', 'split_type', 'custom_amount', 'remaining_share', 'paid_status')
+    list_display = ('id',
+                    'expense_line',
+                    'contributor',
+                    'split_type',
+                    'custom_amount',
+                    'remaining_share',
+                    'paid_status'
+                    )
     readonly_fields = ('remaining_share', 'split_type')
 
     def remaining_share(self, obj):
