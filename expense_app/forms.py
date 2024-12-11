54b73cb7d461f1164d2a649b270888b495344474
Jack Brosnan <ik7pdsoq@students.codeinstitute.net>
2024-12-11 18:00:42 +0000

    Updated forms.py to comply with PEP8. Inserted comments where necessary

--- expense_app/forms.py
+++ expense_app/forms.py
@@ -22,38 +22,37 @@ class ExpenseSpaceForm(forms.ModelForm):
         """
         model = ExpenseSpace
         fields = ['name', 'description', 'space_image', 'currency']
-        
-        """
-        **Widgets**:
-
-        1. ``name``: TextInput with a placeholder for entering the item name.
-        2. ``description``: TextInput with a placeholder for entering the description.
-        3. ``space_image``: FileInput for uploading an image.
-        4. ``currency``: Select field for selecting the currency.
-        """
-
         widgets = {
-                'name': forms.TextInput(
-                    attrs={'placeholder': 'Enter Title'}
-                ),
-                'description': forms.TextInput(
-                    attrs={'placeholder': 'Enter Description'}
-                ),                
-            }
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Title'}),
+            'description': forms.TextInput(
+                attrs={'placeholder': 'Enter Description'}
+            ),
+        }
+
 
 class ContributorForm(forms.ModelForm):
+    """
+    Form for adding or editing a contributor.
+    """
     class Meta:
+        """
+        **Model**: :model:`Contributor`
+
+        **Fields**:
+
+        1. ``name``: TextInput for entering the item name.
+
+        """
         model = Contributor
-        fields = ['name',]
+        fields = ['name', ]
         widgets = {
-                'name': forms.TextInput(
-                    attrs={'placeholder': 'Enter Name'}
-                ),
+            'name': forms.TextInput(attrs={'placeholder': 'Enter Name'}),
         }
+
 
 class ExpenseLineForm(forms.ModelForm):
     """
-    Form for adding or editing an expense.
+    Form for adding or editing an expense line.
     """
     class Meta:
         """
@@ -61,71 +60,102 @@ class ExpenseLineForm(forms.ModelForm):
 
         **Fields**:
 
-        1. ``title``: Text field for entering the item name.
+        1. ``title``: TextInput for entering the item name.
         2. ``description``: TextInput for entering a description.
-        3. ``due_date``: Date input.
-        4. ``amount``: Float field to enter the expense amount.
+        3. ``amount``: Decimal Field to enter the expense amount.
+
+        **Widgets**
+
+        3. ``amount``: min and step attributes to avoid negative
+        and null inputs
 
         """
         model = ExpenseLine
-        fields = ['title', 'description', 'amount', 'due_date']
-        
+        fields = ['title', 'description', 'amount', ]
         widgets = {
-                'title': forms.TextInput(
-                    attrs={'placeholder': 'Enter Title'}
-                ),
-                'description': forms.TextInput(
-                    attrs={'placeholder': 'Enter Description'}
-                ),
-                'amount': forms.NumberInput(
-                    attrs={'placeholder': 'Enter amount', 'min': '0', 'step': '0.01'}
-                ),
-                'due_date': forms.DateInput(
-                    attrs={'placeholder': 'Optional'}
-                ),
-            }
-
-class CustomAmountForm(forms.ModelForm):
-    class Meta:
-        model = Contribution
-        fields = ['custom_amount',]
-
-        widgets = {
-                'custom_amount': forms.NumberInput(
-                    attrs={'placeholder': 'Enter Custom Amount', 'min': '0', 'step': '0.01'}
-                ),
+            'title': forms.TextInput(attrs={'placeholder': 'Enter Title'}),
+            'description': forms.TextInput(
+                attrs={'placeholder': 'Enter Description'}
+            ),
+            'amount': forms.NumberInput(
+                attrs={'placeholder': 'Enter amount',
+                       'min': '0',
+                       'step': '0.01'}
+            ),
         }
 
-    # Custom amount form validation
+
+class CustomAmountForm(forms.ModelForm):
+    """
+    Form for adding or editing a custom amount for a contribution.
+    """
+    class Meta:
+        """
+        **Model**: :model:`Contribution`
+
+        **Fields**:
+
+        1. ``custom_amount``: Decimal Field for inputting custom amounts.
+
+        """
+
+        model = Contribution
+        fields = ['custom_amount', ]
+        widgets = {
+            'custom_amount': forms.NumberInput(
+                attrs={'placeholder': 'Enter Custom Amount',
+                       'min': '0',
+                       'step': '0.01'}
+            ),
+        }
 
     def __init__(self, *args, expense_line=None, **kwargs):
+        """
+        Initialize the form with `expense_line` parameter(optional).
+        """
         super().__init__(*args, **kwargs)
         self.expense_line = expense_line
-        self.update_help_text()
+        self.update_amount_text()
 
-    def update_help_text(self):
+    def update_amount_text(self):
+        """
+        Update the text to show the remaining amount allowed.
+        """
         if self.expense_line:
-            remaining_amount = self.calculate_remaining_amount()
+            remaining_amount = self.get_remaining_amount()
             self.fields['custom_amount'].help_text = f"{remaining_amount}"
 
-    def calculate_remaining_amount(self):
+    def get_remaining_amount(self):
+        """
+        Calculate the remaining amount for custom amount input.
+        """
         total_custom_amount = self.get_total_custom_amount()
         return max(self.expense_line.amount - total_custom_amount, 0)
 
     def get_total_custom_amount(self):
-        contributions = self.expense_line.expense_contributions.exclude(pk=self.instance.pk)
-        return sum(c.custom_amount or 0 for c in contributions)
+        """
+        Calculate the total custom amount for other contributions.
+        """
+        contributions = self.expense_line.expense_contributions
+        filtered_contributions = contributions.exclude(pk=self.instance.pk)
+        return sum(c.custom_amount or 0 for c in filtered_contributions)
 
     def clean_custom_amount(self):
+        """
+        Validate custom amount to ensure it doesn't exceed the limit.
+        """
         custom_amount = self.cleaned_data.get('custom_amount', 0)
         if custom_amount is None:
             return custom_amount
 
         total_custom_amount = self.get_total_custom_amount() + custom_amount
-        if self.expense_line and total_custom_amount > self.expense_line.amount:
+        if (
+            self.expense_line
+            and total_custom_amount > self.expense_line.amount
+        ):
             currency = self.expense_line.expense_space.get_currency_display()
             raise forms.ValidationError(
                 f"Custom amount exceeds the remaining allowable amount. "
-                f"Maximum allowed: {currency} {self.calculate_remaining_amount()}"
+                f"Maximum allowed: {currency} {self.get_remaining_amount()}"
             )
         return custom_amount
